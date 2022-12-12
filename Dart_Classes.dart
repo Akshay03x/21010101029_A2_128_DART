9663bccd8aff85907332c1a529ae19bf8b0d0518
akshay <21010101029@darshan.ac.in>
2022-12-12 21:55:24 +0530

    .

--- Dart_Classes.dart
+++ Dart_Classes.dart
@@ -2,9 +2,17 @@ class Person {
   String? name;
   int? age;
   //constructor
-  Person(String name, int age) {
+  Person(String? name, int age) {
     this.name = name;
     this.age = age;
+  }
+
+  void namedPerameter({required int a, required int b}) {
+    print(a + b);
+  }
+
+  void optionalPerameter(int a, [String ?v, String? r]) {
+    print("string $v,string2 $r,int $a");
   }
 
   //Method
@@ -17,9 +25,10 @@ class Person {
 void main(List<String> args) {
   //Create  Person Class
 
-  Person p = Person('name', 10);
+  Person p = Person(null, 10);
   p.ShowData();
-
+  p.namedPerameter(a: 1, b: 2);
+  p.optionalPerameter(1);
   Person p2 = Person('Jadoo', 1000000);
   p2.ShowData();
 }
--- Dart_Logic.dart
+++ Dart_Logic.dart
@@ -2,6 +2,7 @@ void main(List<String> args) {
   //Logic if IF/else ,If/Else If
   // > < >= <= !=
   var num = 4;
+  assert(num != 5);
   if (num == 5) {
     print('true');
   } else if (num == 4) {
@@ -9,4 +10,7 @@ void main(List<String> args) {
   } else {
     print('false');
   }
+  String s1 = " This is an assert statement ! " ;
+  assert( s1 == "   Thiasaskjaks is an assert statement ! ", " Cool ! The strings are equal. " ) ;
+  print( " You Can See This Line on console as an output if the assert returns true. " ) ;
 }
