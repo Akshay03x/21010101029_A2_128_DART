c306c9d4da3941f25f3b4ef4066d4016830598c5
akshay <21010101029@darshan.ac.in>
2022-12-18 16:41:39 +0530

    .

--- LAB-5/Program_3.dart
+++ LAB-5/Program_3.dart
@@ -1,14 +1,21 @@
-// Write a dart code to create a Circle class with area() and perimeter() function to find area and 
+// Write a dart code to create a Circle class with area() and perimeter() function to find area and
 // perimeter of the circle.
-class Circle{
-  void area(){
+import 'dart:io';
+import 'dart:math';
 
+class Circle {
+  void area(r) {
+    print("${pi * r * r}");
   }
 
-  void perimeter(){
-    
+  void perimeter(r) {
+    print("${pi * 2 * r}");
   }
 }
+
 void main(List<String> args) {
-  
+  Circle c = Circle();
+  print(("Enter Redius"));
+  c.area(double.parse(stdin.readLineSync()!));
+  // c.perimeter(double.parse(stdin.readLineSync()!));
 }
